90e920875df0da36a6265d28b54163c37a0d3c09
BuildingMyEmpire <42008710+BuildingMyEmpire@users.noreply.github.com>
2018-08-01 14:36:26 -0400

    Uploading Website

---
+++ anousha_brianna.js
@@ -0,0 +1,8 @@
+
+window.alert("BEWARE This website contains frightening content");
+function myFunction() {
+  document.getElementById("demo").innerHTML = "There is so much that we don't know about the world. As human beings we are internally functioned to learn something new everyday. However you don't realize that your learning something you just act on instinct. I personally want to know and learn everything. I find myself always asking why. You the statement curiousity killed the cat? How did it kill the cat? Did know it too much? I would rather ask why then be forever clueless about the world around me. Being curious might have gotten someone killed but I am still alive and I want to why. The real question is are you curious enough to what is out there?";
+}
+function show(){
+  document.getElementById("press").style.display = "block";
+}
